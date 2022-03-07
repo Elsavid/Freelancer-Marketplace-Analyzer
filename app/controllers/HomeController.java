99ce46a325ec2cfdc1148f5465164c5486b75288
Yan Ren <ryan.ren3@gmail.com>
2022-03-06 23:38:32 -0500

    Feature main page (#3)
    * merge main into Wenshu;
    update .gitinore for idea
    
    * referring to form example
    
    * delete Constructor
    
    * modify business code;
    delete unused API, routes, and templates;
    
    * add working main page
    
    Co-authored-by: Darius <754434446@qq.com>

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -1,29 +1,48 @@
 package controllers;
 
-import play.mvc.*;
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+import actors.SearchActor;
+import akka.actor.ActorSystem;
+import akka.stream.Materializer;
+import play.cache.AsyncCacheApi;
+import play.libs.streams.ActorFlow;
+import play.mvc.Controller;
+import play.mvc.Http;
+import play.mvc.Result;
+import play.mvc.WebSocket;
+import services.ApiService;
+import play.libs.ws.WSClient;
+import play.libs.ws.WSRequest;
 
 /**
  * This controller contains an action to handle HTTP requests
  * to the application's home page.
  */
+@Singleton
 public class HomeController extends Controller {
+    private AsyncCacheApi cache;
+    private final ActorSystem actorSystem;
+    private final Materializer materializer;
+    @Inject
+    ApiService apiService;
+    @Inject
+    WSClient ws;
 
-    /**
-     * An action that renders an HTML page with a welcome message.
-     * The configuration in the <code>routes</code> file means that
-     * this method will be called when the application receives a
-     * <code>GET</code> request with a path of <code>/</code>.
-     */
-    public Result index() {
-        return ok(views.html.index.render());
-    }
-    
-    public Result explore() {
-        return ok(views.html.explore.render());
-    }
-    
-    public Result tutorial() {
-        return ok(views.html.tutorial.render());
+    @Inject
+    public HomeController(
+            ActorSystem actorSystem, Materializer materializer) {
+        this.actorSystem = actorSystem;
+        this.materializer = materializer;
     }
 
+    public Result index(Http.Request request) {
+        return ok(views.html.index.render(request));
+    }
+
+    public WebSocket ws() {
+        return WebSocket.Json.accept(request -> ActorFlow.actorRef(out -> SearchActor.props(out, apiService),
+                actorSystem, materializer));
+    }
 }
---
+++ app/services/ApiService.java
@@ -0,0 +1,61 @@
+package services;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
+
+import javax.inject.Inject;
+
+import com.fasterxml.jackson.databind.JsonNode;
+
+import models.Project;
+import play.libs.ws.WSBodyReadables;
+import play.libs.ws.WSClient;
+import play.libs.ws.WSRequest;
+
+public class ApiService {
+
+    @Inject
+    WSClient ws;
+
+    public static String listProjects = "https://www.freelancer.com/api/projects/0.1/projects/active?limit=10&job_details=true";
+    public static SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+
+    public CompletionStage<List<Project>> getProjects(String query) {
+        CompletableFuture<Object> resp = sendRequest(listProjects + "&query=\"" + query + "\"");
+        CompletionStage<List<Project>> projectList = processProjectResponse(resp);
+        return projectList;
+    }
+
+    public CompletableFuture<Object> sendRequest(String url) {
+        System.out.println("[debug] sending request: " + url);
+        WSRequest request = ws.url(url);
+
+        CompletionStage<JsonNode> jsonPromise = request.get()
+                .thenApply(r -> r.getBody(WSBodyReadables.instance.json()));
+        return jsonPromise.toCompletableFuture().thenApply(json -> {
+            return json;
+        });
+    }
+
+    public CompletionStage<List<Project>> processProjectResponse(CompletableFuture<Object> json) {
+        List<Project> projects = new ArrayList<>();
+        CompletionStage<List<Project>> jsonPromise = json.thenApply(response -> {
+            ((JsonNode) response).get("result").get("projects").forEach(item -> {
+                Project p = new Project();
+                p.setOwnerId(item.get("owner_id").asText());
+                p.setTitle(item.get("title").asText());
+                p.setSubmitDate(dateFormat.format(new Date(item.get("submitdate").asLong() * 1000L)));
+                for (JsonNode skill : item.get("jobs")) {
+                    p.addSkill(skill.get("name").asText());
+                }
+                projects.add(p);
+            });
+            return projects;
+        });
+        return jsonPromise;
+    }
+}
